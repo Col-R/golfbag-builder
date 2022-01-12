dedfc28e91ca38d9902eac868f19424eb9f660d7
Col-R <2colewestin@gmail.com>
2022-01-12 14:36:12 -0600

    finished get and post for create bag

--- flask_app/controllers/bags.py
+++ flask_app/controllers/bags.py
@@ -1,22 +1,22 @@
-from flask_app import app
 from flask import render_template,redirect,session,request, flash
+from flask_app import app
 from ..models import user, bag
 
-@app.route("/addBag")
+@app.route("/bag/create")
 def new_bag():
     if 'user_id' not in session:
         return redirect('/')
     data = {
         "id":session['user_id']
     }
-    return render_template('add_bag.html', user = user.User.get_by_id(data))
+    return render_template("add_bag.html", user = user.User.get_by_id(data))
 
-@app.route('/addBag/new', methods = ['POST'])
+@app.route('/bag/new', methods = ['POST'])
 def create_bag():
     if 'user_id' not in session:
         return redirect('/logout')
     if not bag.Bag.validate_bag(request.form):
-        return redirect('/addBag')
+        return redirect('/bag/create')
     data = {
         "name":request.form["name"],
         "driver":request.form["driver"],
