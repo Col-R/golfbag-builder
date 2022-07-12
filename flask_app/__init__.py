f677af526deaacefaf7bd409f887f6ae9265d828
Col-R <2colewestin@gmail.com>
2022-07-12 13:33:45 -0500

    prepped for deployment

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,5 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY')
+# app.secret_key = os.getenv('SECRET_KEY')
+app.secret_key = os.getenv('insert_key')
 
